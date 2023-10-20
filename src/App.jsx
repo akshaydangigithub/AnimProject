d0f8c4aec167213b72f770e515a258adcdce659d
Hariom dangi <hdangi224@gmail.com>
2023-10-20 18:00:03 +0530

    make the contact page and vendor dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./screens/Homepage";
-import VendorContact from "./screens/vendorContact";
+import VendorContact from "./screens/VendorContact"
 import VendorDashboard from "./screens/VendorDashboard";
 import Track from "./components/Track";
 import UploadLeads from "./components/UploadLeads";
